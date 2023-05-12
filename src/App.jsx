ab7e4c5e5b8af1c4ac5fe5d8fd2666e8f8444d16
kaarenliangg <karenliang9546@gmail.com>
2023-05-12 15:51:34 +1000

    updates to the board with canvas file, rectangle toolbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect} from "react";
+import { fabric } from "fabric";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [canvas, setCanvas] = useState('');
+
+  useEffect(() => {
+    setCanvas(initCanvas());
+  }, []);
+
+  const initCanvas = () => (
+    new fabric.Canvas('canvas', {
+      height: 800,
+      width: 800,
+      backgroundColor: 'white',
+      boxShadow: '8px 4px 13px #F4AAB9',
+    })
+  )
+
+  const addRect = canvi => {
+    const rect = new fabric.Rect({
+      height: 280,
+      width: 200,
+      fill: 'purple'
+    });
+    canvi.add(rect);
+    canvi.renderAll();
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <h1>Test</h1>
+      <div className="toolbar">
+        <button className="add_rect" onClick={() => addRect(canvas)}></button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <br></br>
+      <canvas id='canvas'></canvas>
+    </div>
   )
-}
+  
+};
 
-export default App
+export default App;
